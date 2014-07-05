35c28ce8f1c25c97a628df0d7d8fc7ef638ef554
MJK <mjk@emmjaykay.org>
2014-07-05 13:32:06 -0700

    decksin place

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from random import shuffle
-
+from random import choice
 '''
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
@@ -35,16 +35,59 @@ class Card:
     def __init__(self, suite=None, number=None):
         self.suite = suite
         self.number = number
+        self.numVal = { 
+                        "one":    1,
+                        "two":    2,
+                        "three":  3,
+                        "four" :  4,
+                        "five" :  5,
+                        "six"  :  6,
+                        "seven":  7,
+                        "eight":  8,
+                        "nine" :  9,
+                        "ten"  : 10,
+                        "jack" : 10,
+                        "queen": 10,
+                        "king" : 10
+                      }
+
+    def __str__(self):
+        e = "%s of %s" % (self.number, self.suite)
+        return e
 
 class Deck:
     def __init__(self):
         self.deck = []
-        for i in Suite:
-            for j in Number:
-                self.deck.append(Card(i,j))
+        self.points = 21
+    def append(self, *cards):
+        for c in cards:
+            self.deck.append(c)
+
+    def value(self):
+        v = 0
+        ace = 0
+        for c in self.deck:
+            if c.number is 'ace':
+                ace += 1
+            else:
+                v += c.numVal[c.number]
+            if ace is not 0:
+                if v + 10*ace < self.points:
+                    v += 10 * ace
+                elif v + 1*ace < self.points:
+                    v += 1 * ace
+        return v
 
     def shuffle(self):
         shuffle(self.deck)
+
+    def draw(self):
+        if len(self.deck) is 0:
+            return None
+
+        i = choice(self.deck)
+        self.deck.remove(i)
+        return i
 
     def __str__(self):
         e = ''
@@ -53,9 +96,67 @@ class Deck:
         e = e[:-1]
         return e
 
-def main():
-    d = Deck()
-    d.shuffle()
+class DealerDeck(Deck):
+    def __init__(self):
+        Deck.__init__(self)
+        for i in Suite:
+            for j in Number:
+                self.deck.append(Card(i,j))
 
+class User:
+    def __init__(self):
+        self.name = ""
+        self.deck = Deck()
+        self.hand = Deck()
+
+    def draw(self, deck):
+        self.hand.append(deck.draw())
+        self.hand.append(deck.draw())
+        
+class Dealer(User):
+    def __init__(self):
+        User.__init__(self)
+        self.deck = DealerDeck()
+        self.deck.shuffle()
+        pass
+
+class Human(User):
+    def __init__(self):
+        User.__init__(self)
+        self.purse = 100
+        pass
+
+class Poker:
+    def __init__(self):
+        self.deck = Deck()
+        self.dealer = Dealer()
+        self.human = Human()
+        self.players = []
+    def dealRound(self):
+        self.dealer.draw(self.dealer.deck)
+        self.human.draw(self.dealer.deck)
+
+    def strongestHand(self, *hands):
+        best = Deck()
+        for h in hands:
+            if h.value() > best.value():
+                best = h
+        return best
+
+    def bustedHand(self, *hands):
+        b = []
+        for h in hands:
+            pass
+
+def main():
+    p = Poker()
+    p.dealRound()
+    h = p.strongestHand(p.dealer.hand,p.human.hand)
+    print "Dealer"
+    print p.dealer.hand
+    print "Human"
+    print p.human.hand
+    print "Strongest"
+    print h
 if __name__ == "__main__":
     main()
